9a0fb7c041e13c12292f1c5c3a4a0463d158139c
GENius7G <127296482+GENius7G@users.noreply.github.com>
2026-05-30 07:55:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,4 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  let filePath = req.url === '/' ? '/index.html' : req.url;
-  filePath = path.join(__dirname, filePath);
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      fs.readFile(path.join(__dirname, 'index.html'), (e, d) => {
-        res.end(d || 'Not found');
-      });
-      return;
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(data);
-  });
-});
-
-server.listen(PORT, () => console.log('Server running on port ' + PORT));
+pi-store
+GENius7G/pi-store
+genius7g.github.io
+GENius7G/genius7g.github.io
